c31644bf1f18d44098032fa938bf3c1b5310226c
Brandon Jordan <brandonjordan124@gmail.com>
2025-09-30 00:01:44 -0400

    Add formula

---
+++ cherri.rb
@@ -0,0 +1,18 @@
+class Cherri < Formula
+  desc "Siri Shortcuts programming language 🍒"
+  homepage "https://cherrilang.org/"
+  license "GPL-2.0"
+  head "https://github.com/electrikmilk/cherri.git"
+
+  # depends_on "go" => :build
+
+  def install 
+    system "go", "build"
+    bin.install "cherri"
+  end
+
+  test do
+    system "#{bin}/cherri", "-v"
+  end
+end
+
