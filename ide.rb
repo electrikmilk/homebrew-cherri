96b7a28398bceecf9cc2bfafe0ed16706b66ffae
Brandon Jordan <brandonjordan124@gmail.com>
2025-10-03 19:31:04 -0400

    Update ide.rb

--- ide.rb
+++ ide.rb
@@ -1,5 +1,5 @@
 class IDE < Formula
-  desc "A macOS IDE for the Cherri programming language."
+  desc "macOS Document-based IDE for the Cherri programming language."
   homepage "https://cherrilang.org/"
   url "https://github.com/electrikmilk/cherri-macos-app/archive/refs/tags/1.1.0.tar.gz"
   sha256 "d965f60549e8739220c60d34be46febe3d047bd3"
@@ -8,7 +8,7 @@ class IDE < Formula
   depends_on xcode: ["15.0", :build]
 
   def install
-    system "xcodebuild", "archive",
+    xcodebuild "archive",
            "-scheme", "Cherri",
            "-configuration", "Release",
            "-destination", "platform=macOS,arch=arm64",
