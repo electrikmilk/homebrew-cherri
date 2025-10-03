eb028bc66568b50da4683aafda28f9507fe969fc
Brandon Jordan <brandonjordan124@gmail.com>
2025-10-03 19:51:02 -0400

    Update cherri-app.rb

--- cherri-app.rb
+++ cherri-app.rb
@@ -2,7 +2,7 @@ class CherriApp < Formula
   desc "macOS IDE for the Cherri programming language"
   homepage "https://cherrilang.org/"
   url "https://github.com/electrikmilk/cherri-macos-app/archive/refs/tags/1.1.0.tar.gz"
-  sha256 "22a0fe9f18b01f6bfe8754616cb362c479f7ad488a3dfcbe35c975ad4ea39d8a"
+  sha256 "03163adf4078dbb77ddaf651c0d08e9750ff369f11d233bf00d5e86cee262338"
   license "GPL-2.0-only"
 
   depends_on xcode: ["15.0", :build]
