3693edfc8f1624056a84a70ba87e4bba0ab05308
Brandon Jordan <brandonjordan124@gmail.com>
2025-11-01 13:13:20 -0400

    Update version and SHA256 checksums in cherri.rb

--- cherri.rb
+++ cherri.rb
@@ -1,19 +1,19 @@
 class Cherri < Formula
   desc "Siri Shortcuts programming language"
   homepage "https://cherrilang.org/"
-  version "1.3.0"
+  version "1.3.1"
   license "GPL-2.0-only"
 
   # macOS binaries
   on_macos do
     on_arm do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_darwin-arm64.zip"
-      sha256 "4be5e3a9e03939f28d4188bd8eef4de6083c4c67619f4a51b3147e792b125b7d"
+      sha256 "adf61502a8f44e23f6a8c409d550e76fef17960058c88048c3c9b1cdccbc20e2"
     end
 
     on_intel do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_darwin-x86_64.zip"
-      sha256 "84b4205adf437addb7d9c8bcb1b309d78cd5c6c1c5f61e37efaa410ffee5febd"
+      sha256 "199e9efb8867cfabe63f796a17ae08f5d7a28b092672fa0294e892f10f4d8d36"
     end
   end
 
@@ -21,12 +21,12 @@ class Cherri < Formula
   on_linux do
     on_arm do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_linux-arm64.zip"
-      sha256 "b6ebe2291e269747e8192cfaf420d9cea21ecf6f493fab726d52e82c087331dc"
+      sha256 "1de676ef839724dec591b1cbcaf75546a3561bbbb824921e4fffe83371d55fe4"
     end
 
     on_intel do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_linux-x86_64.zip"
-      sha256 "62fb939f5e698e480434aad8da03b14bd8c72551bba455cdf18d7e3d7630f1ce"
+      sha256 "39add330f90bfd533395825114e045d94b7a1f14c508411b201eb41d8071077b"
     end
   end
 
