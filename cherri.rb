3e2f4a1e76b95a12beb75b40343d4842c18c2bba
Brandon Jordan <brandonjordan124@gmail.com>
2026-03-31 20:39:14 -0400

    Update SHA256 checksums for macOS and Linux binaries

--- cherri.rb
+++ cherri.rb
@@ -8,12 +8,12 @@ class Cherri < Formula
   on_macos do
     on_arm do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_darwin-arm64.zip"
-      sha256 "78e526134b7efb423bc7e3cd065760fd7ae054dd40a503dd67daefd81e006d07"
+      sha256 "3ee3325d84ec9c4c1ebb240a736cf113d3261078ad76aa81f45c36b721ef6c04"
     end
 
     on_intel do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_darwin-x86_64.zip"
-      sha256 "74d551b3d5ac45071bdccb8e4aa766e8d505577c7a0e775b65d4be5c25609e89"
+      sha256 "6e1042865f049d602983a3cecac6e41e06937e0e8614f81e0768729a84515644"
     end
   end
 
@@ -21,12 +21,12 @@ class Cherri < Formula
   on_linux do
     on_arm do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_linux-arm64.zip"
-      sha256 "839d27deb6385f973f7ffd6cb9000a39c70f0199390c71183b2bfd908463ba2e"
+      sha256 "32694a9159d98e44c2ddf76ff6dd0a7c8dcbb1fcf6ea146b45a6387684c17eb5"
     end
 
     on_intel do
       url "https://github.com/electrikmilk/cherri/releases/download/v#{version}/cherri_linux-x86_64.zip"
-      sha256 "7ddfbdfff950b56cbc7c6c5433d27ee8581737226ff1dbfa5f7d8c21297d012e"
+      sha256 "a18548235bca623eba3fc6eea9b36f97c8f208bc6e83427e785749b3cd7ac321"
     end
   end
 
